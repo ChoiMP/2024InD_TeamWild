6f3464929a200dc8f244b0c0619a93beb1534d64
ParkGaYeon3 <zero3119@naver.com>
2024-08-03 14:30:42 +0900

    map_move

--- Assets/Script/BackGroud_speed.cs
+++ Assets/Script/BackGroud_speed.cs
@@ -6,26 +6,23 @@ public class BackGroud_speed : MonoBehaviour
 {
     public static BackGroud_speed Instance { get; private set; }
     public float _speed;
+    public float _XPotion=0.0f;
     Transform _myTF;
-    public float speedIncreaseFactor = 0.5f; // 속도를 얼마나 증가시킬지 결정하는 계수
-    public float speedIncreaseInterval = 5f; // 속도 증가 주기 (초 단위)
+    public float _speedIncreaseFactor = 0.5f; // 속도를 얼마나 증가시킬지 결정하는 계수
+    public float _speedIncreaseInterval = 5f; // 속도 증가 주기 (초 단위)
+    public float _xPostion = 0.0f;
+    public float _xMove = 0.0f;
+
+    private void Start()
+    {
+        _myTF = GetComponent<Transform>();
+        // 위치를 직접 설정
+        _myTF.position = new Vector3(_XPotion, 0f, 0f);
+    }
 
     private void Awake()
     {
-        // 싱글톤 인스턴스 초기화
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject); // 씬 전환 시 오브젝트 유지
-        }
-        else
-        {
-            Destroy(gameObject); // 이미 인스턴스가 존재하면 현재 오브젝트 제거
-        }
-
-        _myTF = GetComponent<Transform>();
-        // 위치를 직접 설정
-        _myTF.position = new Vector3(50.9f, 0f, 0f);
+        Instance = this;
 
         // 속도 증가 코루틴 시작
         StartCoroutine(IncreaseSpeedOverTime());
@@ -36,9 +33,10 @@ public class BackGroud_speed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_myTF.position.x > -50.3)
+        _myTF.Translate(Vector2.left * _speed * Time.deltaTime);
+        if (_myTF.position.x < _xPostion)
         {
-            _myTF.Translate(Vector2.left * _speed * Time.deltaTime);
+            _myTF.Translate(Vector2.right * _xMove);
         }
     }
 
@@ -47,10 +45,10 @@ public class BackGroud_speed : MonoBehaviour
         while (true)
         {
             // 지정된 시간 동안 대기
-            yield return new WaitForSeconds(speedIncreaseInterval);
+            yield return new WaitForSeconds(_speedIncreaseInterval);
 
             // 현재 속도의 0.5배만큼 속도 증가
-            _speed += _speed * speedIncreaseFactor;
+            _speed += _speed * _speedIncreaseFactor;
         }
     }
 }
--- Assets/Script/MapGamemanger.cs
+++ Assets/Script/MapGamemanger.cs
@@ -29,15 +29,7 @@ public class MapGamemanger : MonoBehaviour
 
     private void Awake()
     {
-        if (Instance == null)
-        {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
     }
 
     private void Start()
